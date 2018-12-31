de6890878d8291edd966a4f2af0968523b60c199
Alex Rocha <alex.rochas@yahoo.com.br>
2018-12-31 19:18:00 -0200

    feat: add pretty print

--- hunting-words.rb
+++ hunting-words.rb
@@ -1,6 +1,5 @@
-
 input = [
-  'carro', 'cachorro'
+  'carro', 'sapato'
   ]
 
 size = 10
@@ -18,6 +17,22 @@ class Matrix
   def []=(i, j, x)
     @rows[i][j] = x
   end
+
+	def to_pretty_s
+		s = ""
+		i = 0
+		while i < self.column_size
+			s += "\n" if i != 0
+			j = 0
+			while j < self.row_size
+				s += ' ' if j != 0
+				s += self.element(i, j).to_s
+				j += 1
+			end
+			i += 1
+		end
+		s
+	end
 end
 
 matrix
@@ -27,7 +42,7 @@ mutableMatrix = Marshal.load(Marshal.dump(matrix))
 input.each do |word|
   index = []
   either = nil
-  matrix.each_with_index do |e, row, col|  
+  matrix.each_with_index do |e, row, col|
     if word.chars.first == e
       if col + word.size < size # linha
         either = Functional::Either.left([row, col])
@@ -38,9 +53,9 @@ input.each do |word|
       end
     end
   end
-  
+
   chars = word.chars
-  
+
   if either != nil and either.right?
     puts "coluna #{either.right}"
     r,c = either.right
@@ -49,7 +64,7 @@ input.each do |word|
       mutableMatrix.[]= column, r, chars[index]
     end
   elsif either != nil and either.left?
-    puts "linha #{either.left}" 
+    puts "linha #{either.left}"
     r,c = either.left
     (c..c+word.length-1).each_with_index do |row, index|
       puts "#{chars[index]} in #{c},#{row}"
@@ -60,4 +75,4 @@ input.each do |word|
   end
 end
 
-mutableMatrix
+puts mutableMatrix.to_pretty_s
